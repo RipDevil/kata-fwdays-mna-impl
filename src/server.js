08e73459a80a5bb15ba36fc6b973756e2761b916
RipDevil <tiberi64@yandex.ru>
2021-07-09 16:25:35 +0700

    :zap:

--- src/server.js
+++ src/server.js
@@ -1,25 +1,43 @@
-import * as http from "http";
-import { showPaths } from "./utils/show-paths.js";
-import { readFile } from "fs/promises";
-import { exec } from "child_process";
+import * as http from 'http';
+import { showPaths } from './utils/show-paths.js';
+import { readFile } from 'fs/promises';
+import { exec } from 'child_process';
 
 export const startServer = (port = 8080) => {
     server.listen(port, () => {
-        console.log("\nThe server is reachable on:");
+        console.log('\nThe server is reachable on:');
 
-        showPaths((addr) => {
+        showPaths(addr => {
             console.log(`${addr}:${port}`);
         });
     });
 
     setTimeout(() => {
-        exec("curl http://localhost:" + port);
+        exec('curl http://localhost:' + port);
     }, 200);
 };
 
-const server = http.createServer(async (request, response) => {
-    const indexPage = await readFile(new URL("./static/index.html", import.meta.url));
+async function performLogin(request, response) {
+    response.statusCode = 500;
+    response.end();
+}
+
+async function serveContent(request, response) {
+    const indexPage = await readFile(
+        new URL('./static/index.html', import.meta.url)
+    );
 
     response.write(indexPage);
     response.end();
+}
+
+const server = http.createServer(async (request, response) => {
+    if (request.method === 'POST' && request.url === '/login') {
+        await performLogin(request, response);
+    } else if (request.method === 'GET') {
+        await serveContent(request, response);
+    } else {
+        response.statusCode = 400;
+        response.end();
+    }
 });
