b308107a0aa2832d0e96979f38f006b38b2394c8
RipDevil <tiberi64@yandex.ru>
2021-07-09 17:30:17 +0700

    :zap: nonce

--- src/knobs/serve.js
+++ src/knobs/serve.js
@@ -1,11 +1,18 @@
+import { randomUUID } from 'crypto';
 import { readFile } from 'fs/promises';
 
 const _serveContent = async (_, response) => {
     const indexPage = await readFile(
-        new URL('../static/index.html', import.meta.url)
+        new URL('../static/index.html', import.meta.url),
+        { encoding: 'utf-8' }
     );
 
-    response.write(indexPage);
+    const cspNonce = randomUUID();
+    response.setHeader(
+        'Content-Security-Policy',
+        `script-src 'nonce-${cspNonce}'`
+    );
+    response.write(indexPage.replace('NONCE', cspNonce));
     response.end();
 };
 
